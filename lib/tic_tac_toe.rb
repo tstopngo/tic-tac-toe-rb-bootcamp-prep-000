7fd49c2d9a91255b704dadae1fb248494827c58a
Tracy Ngo <tngo1206@gmail.com>
2019-03-06 02:12:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,12 +95,13 @@ end
 def play(board)
   until over?(board) != true
     turn(board)
-  end
-  if won?(board)
+    if won?(board)
     puts "Congrats! #{board[0]}"
   elsif draw?(board)
     puts "It was a draw!"
   end
+  end
+  
 end
 
 
